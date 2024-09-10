465a76e58d41950a94116fd5809052c611091203
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-09-10 09:54:32 -0300

    feat: add vpc module

--- variables.tf
+++ variables.tf
@@ -12,3 +12,35 @@ variable "tags" {
   description = "Tags for resources"
   type        = map(string)
 }
+
+### VPC
+
+variable "cidr_block" {
+  description = "CIDR Block for VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "Avaibility Zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "VPC Public Subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "VPC Private Subnets"
+  type        = list(string)
+}
+
+variable "database_subnets" {
+  description = "VPC database Subnets"
+  type        = list(string)
+}
+
+variable "nat_gateway_active" {
+  description = "Enable or not nat Gateways"
+  type        = bool
+}
